dd339033c1316ff73ac5dc1eb97de687161b374b
KabusIblis <kabusiblis@gmail.com>
2017-03-18 14:43:43 +0000

    wip commit of lobby
    nothing bad happend, at all

---
+++ lua/mapcontrol.lua
@@ -0,0 +1,7 @@
+render.TurnOnToneMapping()
+--random shit for tonemap, someone can work on this i dont know how to hook onto gmod entities via lua 
+
+
+-- setbloomscale 0.2
+-- setautoexposuremax 1
+-- setautoexposuremin 0.8
